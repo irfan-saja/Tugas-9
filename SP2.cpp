96a76d425fbeca2084c9057220fd3d11bc65b7d3
irfan-saja <62018474+irfan-saja@users.noreply.github.com>
2020-06-23 02:53:57 -0700

    Create SP2.cpp

---
+++ SP2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "file1.h"
+using namespace std;
+
+int main()
+{
+	int f,k;
+	cin >> f >> k;
+	fungsi(f,k);
+	
+}
+4.
+#include <assert.h>
+#define FNAME "test.c"
+int main(void)
+{
+#line 777 FNAME
+        assert(2+2 == 5);
+}
+
