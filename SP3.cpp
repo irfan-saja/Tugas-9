eb7e3bd65a426a91cc9424cb9621ca8f7f4e9f8a
irfan-saja <62018474+irfan-saja@users.noreply.github.com>
2020-06-23 02:53:24 -0700

    Create SP3.cpp

---
+++ SP3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MIN(a,b) (((a) < (b)) ? a : b )   //mencari nilai minimum
+int main()
+
+{
+
+ int f,k;
+cin >> f >> k ;
+cout << "Nilai terkecil adalah  : " <<< MIN(f,k) << endl;
+return 0;
+
+}
